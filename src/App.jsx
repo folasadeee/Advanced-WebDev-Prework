77a8d274d57e29911221dfb33bd80fce837dab43
folasadeee <folasade.goddard@gmail.com>
2024-08-02 23:13:21 -0400

    Added support for adding new creators to database

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,25 @@ import './App.css';
 import Homepage from './pages/Homepage/Homepage';
 import CreatorDetailsPage from './pages/CreatorDetailsPage/CreatorDetailsPage';
 import AddCreator from './pages/AddCreator/AddCreator';
+import Navbar from './layout/Navbar'
 
 function Routes() {
   let routes = useRoutes([
     {
+
       path: '/',
       element: <Homepage />,
-      // children: [
-      //   {
-      //     path: 'details',
-      //     element: <CreatorDetailsPage />,
-      //   },
-      // ],
-    },
-    { path: 'about', element: <h1>About</h1> },
+    }, {
+
+      element: <Navbar />,
+      children: [
+        
+        { path: 'about', element: <h1>About</h1> },
     { path: 'details/:userId', element: <CreatorDetailsPage />},
     { path: 'add', element: <AddCreator />},
     { path: '*', element: <h1>Page Not Found</h1> },
-    
+      ],
+    }
   ]);
 
   return routes;
---
+++ src/pages/components/AddCreatorForm.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { supabase } from '../../client';
+
+const AddCreatorForm = () => {
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        console.log(event.target.name.value);
+        console.log(event.target.url.value);
+        console.log(event.target.description.value);
+        console.log(event.target.imageURL.value);
+
+        const { data, error } = await supabase
+        .from('creators')
+        .insert([
+            { 
+                name: event.target.name.value,
+                url: event.target.url.value,
+                description: event.target.description.value,
+                imageURL: event.target.imageURL.value
+            }
+        ])
+
+        if (error) {
+            console.log('error', error);
+        }
+
+
+    }
+
+    return (
+        <>
+
+        <form onSubmit={handleSubmit}>
+        <h1>Text</h1>
+            <label>
+                Name:
+                <input type="text" name="name" id="name" required />
+            </label>
+            <label>
+                Url:
+                <input type="text" name="url" id="url" required />
+            </label>
+            <label>
+                Description:
+                <textarea type="text" name="description" id="description" rows="5" cols="33" required />
+            </label>
+
+            <label>
+                Image URL:
+                <input type="text" name="imageURL" id="imageURL" />
+            </label>
+
+            <button type="submit">Add Creator</button>
+            <button type="reset">Clear</button>
+            </form>
+        </>
+    )
+}
+
+export default AddCreatorForm;
