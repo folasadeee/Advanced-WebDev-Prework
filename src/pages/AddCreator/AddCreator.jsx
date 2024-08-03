9f8611a5bc5cdc5b1e67e1566709378f21ba5e42
folasadeee <folasade.goddard@gmail.com>
2024-08-02 22:19:25 -0400

    Added support for creator details pages

---
+++ src/pages/AddCreator/AddCreator.jsx
@@ -0,0 +1,13 @@
+const AddCreator = () =>
+
+    {
+
+    return (
+        <>
+        <h1>Text</h1>
+        </>
+    )
+
+}
+
+export default AddCreator;
--- src/pages/Homepage/components/CreatorCard/CreatorCard.jsx
+++ src/pages/Homepage/components/CreatorCard/CreatorCard.jsx
@@ -1,12 +1,29 @@
 import styles from './CreatorCard.module.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CreatorCard = (props) => {
 
     return (
         <div className={styles.creatorCard}>
+        <Link
+        to={`/details/${props.id}`}
+        key={props.id}>
         <img src={props.avatar} alt="creator-avatar" />
+        </Link>
+
+        <div className={styles.creatorCardText}>
         <h3>{props.name}</h3>
         <p>{props.description}</p>
+        <Link 
+        to={`/details/${props.id}`}
+        key={props.id}>
+        <button>View Creator</button>
+        </Link>
+            </div>
+
+            
+     
         </div>
     );
 }
